79c72805371eb1f60d56458ad9e5f923db96fc25
Suman Barick <codotronix@gmail.com>
2016-06-30 00:33:22 +0530

    slide test initiated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(function(){
     $('.carousel').carousel('cycle');
+    
+    //when user clicks on .subcat-links
+    $('body').on('click', '.sublink', function(){
+        var thisSubsection = $(this).closest('.subsection');
+                       
+        //mark only this sublink as active
+        thisSubsection.find('.sublink').removeClass('active');
+        $(this).addClass('active');
+        
+        //show only this subcontent
+        thisSubsection.find(".content .subcontent").hide();
+        var subcontentID = $(this).attr('data-content-id');
+        $('#'+subcontentID).show();
+    });
 })
