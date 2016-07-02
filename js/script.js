2cbcce5e1bff3ac51ae55f5aca6453286e1f7966
Suman Barick <codotronix@gmail.com>
2016-07-02 23:06:54 +0530

    closing to completion

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(function(){
     $('.carousel').carousel('cycle');
     
+    //Add anim classes
+    $('.subsection .subcontent').addClass('animated slideInRight');
+    //Show 1st subcontent of all subsection
+    $('.subsection .subcontent:first-child').show();
+    
     //when user clicks on .subcat-links
     $('body').on('click', '.sublink', function(){
         var thisSubsection = $(this).closest('.subsection');
