e542084851396c02c97321d5fc133b36d917a9e7
Suman Barick <codotronix@gmail.com>
2020-04-22 23:09:44 +0530

    the book

---
+++ precache-manifest.5324fcc0be1727117f12706dc6ed1bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a368e8ca45dcbfe75f6c3dfa5cad13",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2bc33f26913399fa1615",
+    "url": "/test/static/css/main.1c02a469.chunk.css"
+  },
+  {
+    "revision": "7c468937ba9d37721111",
+    "url": "/test/static/js/2.61cc7426.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test/static/js/2.61cc7426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc33f26913399fa1615",
+    "url": "/test/static/js/main.ee6f5ce6.chunk.js"
+  },
+  {
+    "revision": "2e497171893303a4a347",
+    "url": "/test/static/js/runtime-main.7c878030.js"
+  }
+]);
